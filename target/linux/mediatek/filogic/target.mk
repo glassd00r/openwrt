ced35a34c6f59bd40ee35a6868616f97e6eeb983
longnt2007 <longnt2007@gmail.com>
2026-06-01 20:45:49 +0700

    mediatek: update mtk patches
    Remove Mediatek HNAT (not compatiable with upstream MTK PPE)
    
    Remove dts patches mt7987
    
    Revert compiler flag O2 back to Os
    
    Update mtk patches from feeds to June-01

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -4,7 +4,7 @@ BOARDNAME:=Filogic 8x0 (MT798x)
 CPU_TYPE:=generic
 CPU_SUBTYPE:=cortex-a73
 ARCH_PACKAGES:=aarch64_cortex-a53
-CFLAGS:=-O2 -pipe -march=armv8-a+crypto+crc -mtune=cortex-a73
+CFLAGS:=-Os -pipe -march=armv8-a+crypto+crc -mtune=cortex-a73
 DEFAULT_PACKAGES += fitblk kmod-crypto-hw-safexcel wpad-openssl uboot-envtools
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=openwrt_one
